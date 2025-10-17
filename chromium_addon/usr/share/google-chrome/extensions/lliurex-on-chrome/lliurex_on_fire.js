29fd16982ca5138dc91deb94f9c628bd6a3f6d97
lliurex <juanma1980@gmail.com>
2025-10-17 09:41:33 +0200

    0.9.7 release

--- chromium_addon/usr/share/google-chrome/extensions/lliurex-on-chrome/lliurex_on_fire.js
+++ chromium_addon/usr/share/google-chrome/extensions/lliurex-on-chrome/lliurex_on_fire.js
@@ -2,31 +2,35 @@
  * Extension that adds Lliurex mods to Firefox/Chromium
  */
 
-var lliurex_bm_url={'https://wiki.edu.gva.es/lliurex/tiki-index.php':'Wiki de LliureX',
-'https://mestreacasa.gva.es/web/lliurex':'',
-'https://portal.edu.gva.es/lliurex/va':'LliureX',
-'https://mestreacasa.gva.es/web/lliurex/forums':'Foro de LliureX',
-'https://portal.edu.gva.es/appsedu/aplicacions/':'AppsEdu'};
-var extra_bm_url={'https://mestreacasa.gva.es/web/guest/inicio':'Mestre a casa'};
-var gva_tools={'https://itaca3.edu.gva.es/escriptori/':'Itaca',
-'https://itaca.edu.gva.es':'',
-'https://aules.edu.gva.es/moodle':'Aules',
-'https://webmail.gva.es':'',
-'https://otrs.edu.gva.es/otrs/customer.pl':'Incidencias',
-'https://ovidoc.edu.gva.es':'OVIDOC',
-'https://appweb.edu.gva.es/SID':'SID',
-'http://www.ceice.gva.es/es/web/centros-docentes/guia-de-centros-docentes':'Guía de Centros',
-'https://appweb.edu.gva.es/InventarioWeb':'Inventario TIC',
-'https://oficinavirtual.gva.es':'OVICE'
+var lliurex_bm_url={
+	'https://mestreacasa.gva.es/web/lliurex/forums':'Foro de LliureX',
+	'https://portal.edu.gva.es/appsedu/':'AppsEdu',
+	'https://wiki.edu.gva.es/lliurex/tiki-index.php':'Wiki de LliureX',
+	'https://portal.edu.gva.es/lliurex/va/':' Blog de LliureX',
+	'https://portal.edu.gva.es/lliurex/va/':'LliureX',
 };
-var gva_sites={'http://www.ceice.gva.es/es':'Web CEICE',
-'https://portal.edu.gva.es/cvtic':'Comunidad CvTIV',
-'http://www.ceice.gva.es/es/web/formacion-profesorado':'CEFIREs',
+var extra_bm_url={
+	'https://portal.edu.gva.es/aules/es/inicio/':'Aules'
 };
-var bm_dict={'LliureX':lliurex_bm_url,'Mestre a casa':extra_bm_url,'Utils. GVA':gva_tools,'GVA sites':gva_sites};
-var bm_treeNode='';
+var gva_tools={
+	'http://www.ceice.gva.es/va/web/centros-docentes/guia-de-centros-docentes':'Guía de Centros',
+	'https://oficinavirtual.gva.es':'OVICE',
+	'https://ovidoc.edu.gva.es':'OVIDOC',
+	'https://portal.edu.gva.es/sai/':'SAI',
+	'https://portal.edu.gva.es/biblioedu/':'BiblioEdu',
+	'https://portal.edu.gva.es/portal/va/inici/':'PortalEdu',
+	'https://itaca.edu.gva.es/':'Itaca',
+};
+var gva_sites={
+	'http://www.ceice.gva.es/es/web/formacion-profesorado':'CEFIREs',
+	'https://portal.edu.gva.es/itedugva/va':'ITEDU',
+	'http://www.ceice.gva.es/es':'Web CEICE',
+};
+var bm_dict={'LliureX':lliurex_bm_url,'Aules':extra_bm_url,'Utils. GVA':gva_tools,'GVA sites':gva_sites};
+var bm_remove=['Mestre a casa'];
 var max_tries=6;
 var bm_tree=[];
+var query="";
 var bookmark_bar_id=0;
 var bookmark_title='';
 
@@ -42,98 +46,6 @@ function redirect(requestDetails)
 		};
 }
 //function redirect
-
-function removeBookmarkFromId(bmTree)
-{
-	for (let [bmT] of Object.entries(bmTree)){
-		if (bmT!=null)
-		{
-			if (bmTree[bmT].id.endsWith("__")==false)
-			{
-				//chrome.bookmarks.remove(bmTree[bmT].id).catch(not_resolved);
-				console.log('Remove url '+bmT+': ' +bmTree[bmT].id);
-			}
-		};
-	};
-
-}
-//function removeBookmarkFromId
-
-function checkBookmarks(bm_item)
-{
-	let bm=bm_item;
-	console.log("Lliurex-on-fire: check bm " + bm.id);
-	let folder_dict=bm_dict[bm.title];
-	for (let [bm_url,bm_name] of Object.entries(folder_dict)){
-		console.log("Lliurex-on-fire: check url " + bm_url);
-		console.log("Lliurex-on-fire: name " + bm_name);
-		chrome.bookmarks.search({'url':bm_url}).then(removeBookmarkFromId,not_resolved).catch(not_resolved);
-		chrome.bookmarks.search({'title':bm_name}).then(removeBookmarkFromId,not_resolved).catch(not_resolved);
-	}
-	for (let [bm_url,bm_name] of Object.entries(folder_dict)){
-			chrome.bookmarks.search({'title':bm_name,'url':bm_url},function helper(bmTree){
-				for (let [bmT] of Object.entries(bmTree)){
-					if (bmT!=null)
-					{
-						console.log('Remove ID '+bmTree[0].id);
-						chrome.bookmarks.remove(bmTree[bmT].id)
-					}
-				}
-				if ((bm_name.length>0)) //&& (bm_name.length>0))
-				{
-					bm_data={'title':bm_name,'url':bm_url,'parentId':bm.id,'index':0};
-					console.log('create '+bm_data['title']);
-					chrome.bookmarks.create(bm_data);
-				}
-			});
-	}
-	var query={'title':bm.title};
-	chrome.bookmarks.search(query,reload);
-}
-//function checkBookmarks
-
-function reload(bm_folder)
-{
-	console.log("Reload: "+bm_folder)
-	if (! bm_folder[0])
-	{
-		if (max_tries)
-		{
-			max_tries=max_tries-1;
-			chrome.runtime.reload();
-		}
-	}
-}
-//function reload
-
-function createBookmarksFolder(title)
-{
-	console.log("Lliurex-on-fire: Add bm folder "+title);
-	chrome.bookmarks.getTree(function(tree){
-		bookmark_bar_id=""+tree[0].children[0].id;
-		bookmark_title=tree[0].children[0].title;
-		if (bookmark_bar_id=="menu________")
-		{
-			console.log("Lliurex-on-fire: Firefox toolbar");
-			bookmark_bar_id='toolbar_____';
-		}
-		console.log("Lliurex-on-fire: Title "+bookmark_title+" id "+bookmark_bar_id);
-		query={'title':title,'parentId':bookmark_bar_id};
-		bm_folder=chrome.bookmarks.create(query,function(newFolder){checkBookmarks(newFolder)})
-		});
-}
-//function createBookmarksFolder
-
-function processBookmarks(bm_folder)
-{
-	if (bm_folder[0])
-	{
-		console.log("Processing "+bm_folder[0].title)
-		checkBookmarks(bm_folder[0]);
-	} 
-
-}
-//function processBookmarks
 
 function resolved(record)
 {
@@ -165,22 +77,23 @@ function loadErrorOcurred(details)
 	//Disabled. Remove "return" to enable
 //	return;
 	id=details.tabId;
-	tabUrl=details.url;
+	let tabUrl=details.url;
 	if (tabUrl.startsWith("moz"))
 	{
 		return;
 	}
 	console.log(tabUrl);
-	frameId=details.frameId;
+	let frameId=details.frameId;
 	let domain=new URL(details.url).hostname;
-	if((typeof(browser.dns=="object")) && (frameId==0))
+	if((typeof(chrome.dns=="object")) && (frameId==0))
 	{
 		console.log(details);
 		try
 		{
 			console.log("Domain: "+domain);
-			browser.dns.resolve(domain,["bypass_cache"]).then(resolved);
+			chrome.dns.resolve(domain,["bypass_cache"]).then(resolved);
 		}catch{
+			console.log(details.rul);
 			not_resolved(details.url);
 		};
 	}else{
@@ -193,41 +106,157 @@ function loadErrorOcurred(details)
 }
 //function loadErrorOcurred
 
-function getBookmarks(bookmarkItem)
+function reload(bm_folder)
 {
-	if (bookmarkItem.type="folder")
+	console.log("Reload: "+bm_folder)
+	if (! bm_folder[0])
 	{
-//		console.log(bookmarkItem);
-		if (bookmarkItem.title)
+		if (max_tries)
 		{
-			bm_tree.push(bookmarkItem.title);
-		}
-		if (bookmarkItem.children)
-		{
-			for (child of bookmarkItem.children)
-			{
-				getBookmarks(child);
-			}
+			max_tries=max_tries-1;
+			//chrome.runtime.reload();
 		}
 	}
 }
+//function reload
 
-function exploreTree(bookmarkItems)
+function checkBookmarks(bm_item)
 {
-	getBookmarks(bookmarkItems[0]);
-	for (name in bm_dict)
-	{
-		console.log("Checking "+name)
-		if (bm_tree.includes(name))
-		{
-			var query={'title':name};
-			chrome.bookmarks.search(query,processBookmarks);
-		}else{
-			createBookmarksFolder(name);
-		}
+	let bm=bm_item;
+	console.log(bm_item[0]);
+	console.log("Lliurex-on-fire: check bm " + bm_item);
+	let folder_dict=bm_dict[bm.title];
+	console.log("Debug checkBookamrks -->");
+	console.log(folder_dict);
+	console.log("Was: "+bm.title);
+	
+	console.log("Debug checkBookamrks --<");
+	for (let [bm_url,bm_name] of Object.entries(folder_dict)){
+			console.log("1: "+bm_url);
+			console.log("2: "+bm_name);
+			chrome.bookmarks.search({'title':bm_name,'url':bm_url}).then(function helper(bmTree){
+				if (bm_name!=null)
+				{
+					var bm_data={'title':bm_name,'url':bm_url,'parentId':bm.id,'index':0};
+					console.log('create '+bm_data['title']);
+					console.log('parentId: '+bm.id);
+					try
+					{
+						chrome.bookmarks.create(bm_data);
+					} catch {
+						not_resolved(bm_data['title']);
+					}
+				}
+			})
 	}
-	console.log(bm_tree);
+	query={'title':bm.title};
+	console.log("Q: "+query.title);
+	chrome.bookmarks.search(query,reload);
 }
+//function checkBookmarks
+
+function createBookmarksFolder(title)
+{
+	console.log("Lliurex-on-fire: Adding bm folder "+title);
+	chrome.bookmarks.getTree(function(tree){
+		console.log("Tree");
+		console.log(tree);
+		var bookmark_bar_id=""+tree[0].children[0].id;
+		var bookmark_title=tree[0].children[0].title;
+		if (bookmark_bar_id=="menu________")
+		{
+			console.log("Lliurex-on-fire: Browser toolbar");
+			bookmark_bar_id='toolbar_____';
+		}
+		console.log("Lliurex-on-fire: Title "+bookmark_title+" id "+bookmark_bar_id);
+		query={'title':title,'parentId':bookmark_bar_id};
+		try
+		{
+			chrome.bookmarks.create(query,checkBookmarks)
+		} catch {
+			not_resolved(query["title"]);
+		}
+
+	});
+	console.log("Processed");
+}
+//function createBookmarksFolder
+
+function findBookmarkFolderByName(name, callback) {
+	if (name.length==0)
+		return;
+	chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+		function search(nodes) {
+			for (let node of nodes) {
+				if (node.title === name && node.children) {
+					callback(node.id);
+					return true; 
+				}
+				if (node.children) {
+					if (search(node.children)) {
+						return true;
+					}
+				}
+			}
+			return false; // Not found
+		}
+		search(bookmarkTreeNodes);
+	});
+}
+//function findBookmarkFolderByName
+
+function removeBookmarkFolder(folderId) {
+	chrome.bookmarks.getChildren(folderId, function(children) {
+		 if (children.length === 0) {
+			try
+			{
+				chrome.bookmarks.remove(folderId, function() {
+					console.log('Removed '+folderId);
+				});
+			} catch {
+				not_resolved("Err removing standalone "+folderId);
+			}
+		} else {
+			let count = children.length;
+			children.forEach(child => {
+				if (child.url) {
+					try
+					{
+						chrome.bookmarks.remove(child.id, function() {
+							count--;
+							if (count === 0) {
+								try
+								{
+									chrome.bookmarks.remove(folderId, function() {
+										console.log('Removed parent '+folderId);
+									 });
+								} catch {
+									not_resolved("Err removing parent "+folderId);
+								}
+							}
+						});
+					} catch {
+						not_resolved("Err removing last "+folderId);
+					}
+				} else {
+					removeBookmarkFolder(child.id);
+				}
+			})
+		}
+	});
+}
+//function removeBookmarkFolder
+
+function removeFolderByName(name) {
+	findBookmarkFolderByName(name, function(folderId) {
+		if (folderId) {
+			removeBookmarkFolder(folderId);
+		} else {
+			console.log('Folder not found');
+		}
+	});
+}
+//function removeFolderByName
 
 /*chrome.webRequest.onBeforeRequest.addListener(
 	redirect,
@@ -240,5 +269,15 @@ chrome.webNavigation.onErrorOccurred.addListener(
 	);
 
 var id=0;
+var name="";
 console.log("Lliurex-on-fire: Init");
-chrome.bookmarks.getTree(exploreTree);
+for (let idx in bm_remove)
+{
+	removeFolderByName(bm_remove[idx]);
+}
+for (let name in bm_dict)
+{
+	removeFolderByName(name);
+}
+for (let name in bm_dict)
+	createBookmarksFolder(name);
